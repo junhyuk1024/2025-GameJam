76eebc70fbb4360e8e8614cda4d6250f6db790c8
Kyungbeom Kim <166625539+ruxxja@users.noreply.github.com>
2025-07-30 15:30:42 +0900

    특정 키 상호작용 및 삭제
    .

--- Assets/Kim Kyungbum/Script/InteractText.cs
+++ Assets/Kim Kyungbum/Script/InteractText.cs
@@ -6,6 +6,17 @@ public class InteractText : MonoBehaviour
     public TextMeshProUGUI promptText;
     private ObjDestroy targetDestroyScript;
 
+    public float PromptAlpha
+    {
+        get => promptText.color.a;
+        set
+        {
+            Color newColor = promptText.color; // 복사본 가져오기
+            newColor.a = value;          // 복사본 수정
+            promptText.color = newColor;       // 복사본을 다시 대입
+        }
+    }
+
     void Start()
     {
         targetDestroyScript = GetComponent<ObjDestroy>();
@@ -14,7 +25,6 @@ public class InteractText : MonoBehaviour
     {
         if (promptText.color.a != 0)
         {
-            Debug.Log(promptText.color.a);
             targetDestroyScript.destroyObject();
         }
     }
@@ -25,6 +35,7 @@ public class InteractText : MonoBehaviour
         {
             Debug.Log("켜짐");
             promptText.color = new Color32(255, 255, 255, 255);
+            Debug.Log(promptText.color.a);
         }
     }
     void OnTriggerExit(Collider other)
@@ -33,6 +44,7 @@ public class InteractText : MonoBehaviour
         {
             Debug.Log("꺼짐");
             promptText.color = new Color32(255, 255, 255, 0);
+            Debug.Log(promptText.color.a);
         }
     }
 }
--- Assets/Kim Kyungbum/Script/ObjDestroy.cs
+++ Assets/Kim Kyungbum/Script/ObjDestroy.cs
@@ -1,16 +1,24 @@
 using UnityEditor;
 using UnityEngine;
+using TMPro;
 
 public class ObjDestroy : MonoBehaviour
 {
     [SerializeField]
     private GameObject targetObj;
+    public InteractText interactText;
+    void Start()
+    {
+        interactText = GetComponent<InteractText>();
+    }
     public void destroyObject()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             Destroy(targetObj);
-            Debug.Log("삭제됨!!");
+            interactText.PromptAlpha = 0.0f;
+            Debug.Log("promptText 투명도: " + interactText.PromptAlpha);
+            Debug.Log("오브젝트 삭제됨");
         }
     }
 }

2e98dd99c47fd60971bdb0b4589324ba444823cb
amatna <hmin220@naver.com>
2025-07-31 03:29:38 +0900

    07-31 03-28
    배경음악 관련

--- Assets/Lee Hyeongmin/Scripts/AudioManager.cs
+++ Assets/Lee Hyeongmin/Scripts/AudioManager.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class AudioManager : BaseSingleton<AudioManager>
 {
+    public AudioSource catAudioSource;
+    public AudioSource snakeAudioSource;
+    public AudioSource birdAudioSource;
+    public AudioSource tigerAudioSource;
+    public AudioSource monkeyAudioSource;
+    public bool PlayingLobbyBGM;
+
     private AudioSource audioSource;
 
     protected override void Awake()
@@ -13,5 +20,14 @@ public class AudioManager : BaseSingleton<AudioManager>
     public void PlayOneSound(AudioClip audioClip)
     {
         audioSource.PlayOneShot(audioClip);
+    }
+
+    public void PlayLobbyBGM()
+    {
+        if (GameManager.Instance.hasCat) catAudioSource.Play();
+        if (GameManager.Instance.hasSnake) snakeAudioSource.Play();
+        if (GameManager.Instance.hasBird) birdAudioSource.Play();
+        if (GameManager.Instance.hasTiger) tigerAudioSource.Play();
+        if (GameManager.Instance.hasMonkey) monkeyAudioSource.Play();
     }
 }
